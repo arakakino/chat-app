343015f236a24df3d938266c37c86df12a8c8c8d
arakakino <etffxfang@gmail.com>
2020-09-09 15:03:09 +0900

    フロント実装の準備

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,8 @@ module ChatApp
       g.javascripts false
       g.helper false
       g.test_framework false
-    ennd
-    
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
